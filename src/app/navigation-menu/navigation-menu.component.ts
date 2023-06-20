ed3f083f411f3e0ed77b77ee6c70ba431695714a
Nick Zambelli <nicholas.zambelli@firemon.com>
2023-06-20 16:41:38 -0400

    Tunes navigation menu and styling
    Now works more smoothly on all screen sizes and adjusts to use available
    space more effectively.

--- src/app/navigation-menu/navigation-menu.component.ts
+++ src/app/navigation-menu/navigation-menu.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, HostListener, ViewChild } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { MatIconModule } from "@angular/material/icon";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @Component({
   standalone: true,
@@ -7,7 +9,28 @@ import { RouterModule } from "@angular/router";
   templateUrl: './navigation-menu.component.html',
   styleUrls: ['./navigation-menu.component.scss'],
   imports: [
+    BrowserAnimationsModule,
+    MatIconModule,
     RouterModule,
   ]
 })
-export class NavigationMenuComponent {}
+export class NavigationMenuComponent {
+  public showNavOptions: boolean = true;
+
+  @ViewChild('navOptions') navOptions: ElementRef;
+  @ViewChild('portfolio') portfolio: ElementRef;
+  @ViewChild('connect') connect: ElementRef;
+  @ViewChild('settings') settings: ElementRef;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    // hide the menu button and remove the showing class from the menu items
+    if (!this.showNavOptions && event.target.innerWidth > 750) {
+      this.showNavOptions = true;
+    }
+  }
+
+  public toggleMenuOptions = () => {
+    this.showNavOptions = !this.showNavOptions;
+  }
+}
