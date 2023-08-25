0103a6a7a0c0257828579bf889923295c30d8344
Zambelli24 <zambeezy008@gmail.com>
2023-08-25 08:28:08 -0400

    Adds toggle for changing fonts and styles

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
 import { ColorThemeService } from '../services/color-theme.service';
+import { FontService } from '../services/font.service';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   standalone: true,
@@ -8,18 +12,31 @@ import { ColorThemeService } from '../services/color-theme.service';
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
   imports: [
+    FormsModule,
+    MatFormFieldModule,
     MatSlideToggleModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ],
 })
 export class SettingsComponent {
+  fontSelector = new FormControl();
+  fontStyle: string;
   isDarkMode: boolean;
 
   constructor(
     protected colorThemeService: ColorThemeService,
+    protected fontService: FontService,
   ) {
+    this.fontSelector.setValue(this.fontService.getFontFamily());
     this.isDarkMode = this.colorThemeService.getDarkModeState();
+    this.fontSelector.valueChanges.subscribe((value) => this.changeFont(value))
   }
 
+  changeFont = (font: string) => {
+    this.fontService.changeFontFamily(font);
+  }
+  
   changeTheme = () => {
     this.colorThemeService.changeTheme();
   }
